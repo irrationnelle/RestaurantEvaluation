e189fdad87308db3cb691747e9b3bb4d912efe6a
irrationnelle <drakkarverenis@gmail.com>
2016-11-24 03:04:15 +0900

    add menulist in tmain

--- RestaurantEvaluation/RestaurantEvaluation.cpp
+++ RestaurantEvaluation/RestaurantEvaluation.cpp
@@ -10,6 +10,21 @@ using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {			
-    return 0;
+	cout << "음식점 추천 프로그램: \n" << endl;
+	
+	cout << "1. 음식점 등록" << endl;
+	cout << "2. 만족도 입력" << endl;
+	cout << "3. 음식점 추천" << endl;
+	cout << "4. 음식점 추천(우수)\n" << endl;
+	cout << "프로그램 종료: q\n" << endl;
+	cout << "선택: " << endl;
+	int num;
+	cin >> num;
+
+	// 어떤 클래스의 함수에 인자로 num을 넘겨준다.
+
+	cout << num << endl;
+
+	return 0;
 }
 
